2ce91d948683f31f249086a8a1439c3a607c4cbc
Sean Murphy <msean@vmware.com>
2016-07-18 10:34:52 -0500

    Update clone_and_link.sh
    - Add if statement to check for git-smart

--- files/bin/clone_and_link.sh
+++ files/bin/clone_and_link.sh
@@ -3,8 +3,16 @@ pushd "$HOME"
 
   if [ -d ".dot-files" ]; then
     pushd ".dot-files"
-      git smart-pull
-      git submodule update --recursive
+      #Check if git-smart installed
+      gem list -l git-smart | grep -q git-smart
+      if [ $? == 0 ]; then
+        git smart-pull
+        git submodule update
+      else
+        git fetch
+        git pull --ff-only
+        git submodule update
+      fi
     popd
   else
     git clone --recursive "git://github.com/soylentgrn/dot-files.git" ".dot-files"
