3cb9df01585fd780305d17c8db3c4bcbcb38cafa
Sean Murphy <soylent@AutomatingSecOps.com>
2023-03-05 14:09:40 -0600

    Refactor

--- bin/naclient-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#sudo installer -pkg /Users/msean/Documents/naclient.pkg -target /
-sudo installer -pkg /Users/msean/Downloads/naclient.pkg -target /
-cp /Users/msean/Documents/naclient.conf /opt/sslvpn-plus/naclient/naclient.conf
